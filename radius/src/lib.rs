ce368bff91e64bc3887be35565ffdb89e5b8c502
Geoffroy Couprie <geo.couprie@gmail.com>
2017-05-17 18:28:03 +0200

    import assets in tests

--- radius/src/lib.rs
+++ radius/src/lib.rs
@@ -2,6 +2,11 @@
 
 #[cfg(test)]
 mod tests {
+    const access_request   : &[u8] = include_bytes!("../../assets/radius-access-request.bin");
+    const access_challenge : &[u8] = include_bytes!("../../assets/radius-access-challenge.bin");
+    const access_reject    : &[u8] = include_bytes!("../../assets/radius-access-reject.bin");
+    const access_accept    : &[u8] = include_bytes!("../../assets/radius-access-accept.bin");
+
     #[test]
     fn it_works() {
     }
